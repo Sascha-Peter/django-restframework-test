0bbc90170eaa6a3da877d77498ec15aa625a9421
Sascha Peter <sascha.peter@stickyboard.co.uk>
2017-01-15 19:43:37 +0000

    fix: dev: Adjust some typos in project

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from tutorial.uickstart.serializers import UserSerializer, GroupSerializer
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
