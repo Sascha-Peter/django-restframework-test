9e8619bfdd03e4cf3143018fe6d69b4d394d1d6b
Sascha Peter <sascha.peter@stickyboard.co.uk>
2017-01-23 21:47:20 +0000

    fix: dev: Adjust serializer as modelserializer

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,38 +1,13 @@
 """Define serializers for the snippet app."""
 from rest_framework import serializers
-from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from snippets.models import Snippet
 
 
-class SnippetSerializer(serializers.Serializer):
+class SnippetSerializer(serializers.ModelSerializer):
     """Define the serializer for the snippet model."""
 
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(
-        required=False,
-        allow_blank=True,
-        max_length=100
-    )
-    code = serializers.CharField(style={'base_template': 'textarea.html'})
-    linenos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(
-        choices=LANGUAGE_CHOICES,
-        default='python'
-    )
-    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+    class Meta:
+        """Meta definition for SnippetSerializer."""
 
-    def create(self, validated_data):
-        """Create and return a new Snippet instance with the validated_data."""
-        return Snippet.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """Update and return existing Snippet instance with the validated_data.
-
-        Updates all fields, saves the instance and returns it.
-        """
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.languge = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-        return instance
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
