301518c5b091142f46bf357e0613f0b3dd3544c1
Sascha Peter <sascha.peter@stickyboard.co.uk>
2017-01-23 22:00:30 +0000

    new: dev: Add view for snippet GET/POST

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+"""Define views for the restful api."""
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
 
-# Create your views here.
+
+class JSONResponse(HttpResponse):
+    """Define HTTPResponse that renders it's content into json."""
+
+    def __init__(self, data, **kwargs):
+        """Define initial values for instance object."""
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+
+@csrf_exempt
+def snippet_list(request):
+    """Get a list of all snippets or create a new snippet."""
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(dat=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
