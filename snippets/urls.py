15104bc6a71a487985ae572718d4fa8fba895ef0
Sascha Peter <sascha.peter@stickyboard.co.uk>
2017-01-24 19:51:34 +0000

    fix: dev: Adjust api for tutorial to only include snippets for now

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,7 +4,7 @@ from snippets import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    url(r'^$', views.snippet_list),
+    url(r'^snippets/$', views.snippet_list),
     url(r'^(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
 
