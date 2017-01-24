ef811faa99fb82eeeb7a417cffb201d72a09a5f2
Sascha Peter <sascha.peter@stickyboard.co.uk>
2017-01-24 20:04:31 +0000

    fix: dev: Refactor views for class based views

--- snippets/views.py
+++ snippets/views.py
@@ -1,25 +1,33 @@
 """Define views for the restful api."""
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from django.http import Http404
 from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
-@api_view(['GET', 'POST'])
-def snippet_list(request, format=None):
-    """Get a list of all snippets or create a new snippet.
+class SnippetList(APIView):
+    """Class based implementation of the snippet list view."""
 
-    Arguments:
-        request -- HTTP request
-        format -- string, format of API output/response
-    """
-    if request.method == 'GET':
+    def get(self, request, format=None):
+        """Implement GET method for snippet list.
+
+        Arguments:
+            request -- HTTP request to snippet list view
+            format -- string, format of response
+        """
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
+        """Implement POST method for snippet list.
+
+        Arguments:
+            request -- HTTP requst for snippet list view
+            format -- string, format of response
+        """
         serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -27,31 +35,55 @@ def snippet_list(request, format=None):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'POST', 'DELETE'])
-def snippet_detail(request, pk, format=None):
-    """Get, update or delete a code snippet.
+class SnippetDetail(APIView):
+    """Class based implementation of the snippet detail view."""
 
-    Arguments:
-        request -- HTTP request
-        pk -- int, primary key of object to fetch/return
-        format -- string, format of API output/response
-    """
-    try:
-        snippet = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    def get_object(self, pk):
+        """Implement get of 404.
 
-    if request.method == 'GET':
+        Arguments:
+            pk -- int, identifier of snippet to get
+        """
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        """Implement GET method for snippet detail view.
+
+        Arguments:
+            request -- HTTP request
+            pk -- int, identifier of snippet to get
+            format -- string, format of response
+        """
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        """Implement PUT method for snippet detail view.
+
+        Arguments:
+            request -- HTTP request
+            pk -- int, identifier of snippet to update
+            format -- string, format of response
+        """
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        """Implement DELETE method for snippet detail view.
+
+        Arguments:
+            request -- HTTP request
+            pk -- int, identifier of snippet to delete
+            format -- string, format of response
+        """
+        snippet = self.get_object(pk)
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
