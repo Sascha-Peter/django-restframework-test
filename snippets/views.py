4034744b7e44033bc61ceb6cdb7ddebcfda4cb24
Sascha Peter <sascha.peter@stickyboard.co.uk>
2017-01-25 21:50:10 +0000

    fix: dev: Apply mixins to all models

--- snippets/views.py
+++ snippets/views.py
@@ -1,10 +1,6 @@
 """Define views for the restful api."""
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from django.http import Http404
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework import mixins
 from rest_framework import generics
 
@@ -37,58 +33,32 @@ class SnippetList(mixins.ListModelMixin,
 class SnippetDetail(mixins.RetrieveModelMixin,
                     mixins.UpdateModelMixin,
                     mixins.DestroyModelMixin,
-                    generic.GenericAPIView):
+                    generics.GenericAPIView):
     """Class based implementation of the snippet detail view."""
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get_object(self, pk):
-        """Implement get of 404.
-
-        Arguments:
-            pk -- int, identifier of snippet to get
-        """
-        try:
-            return Snippet.objects.get(pk=pk)
-        except Snippet.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
+    def get(self, request, *args, **kwargs):
         """Implement GET method for snippet detail view.
 
         Arguments:
             request -- HTTP request
-            pk -- int, identifier of snippet to get
-            format -- string, format of response
         """
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet)
-        return Response(serializer.data)
+        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, pk, format=None):
+    def put(self, request, *args, **kwargs):
         """Implement PUT method for snippet detail view.
 
         Arguments:
             request -- HTTP request
-            pk -- int, identifier of snippet to update
-            format -- string, format of response
         """
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return self.update(request, *args, **kwargs)
 
-    def delete(self, request, pk, format=None):
+    def delete(self, request, *args, **kwargs):
         """Implement DELETE method for snippet detail view.
 
         Arguments:
             request -- HTTP request
-            pk -- int, identifier of snippet to delete
-            format -- string, format of response
         """
-        snippet = self.get_object(pk)
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return self.destroy(request, *args, **kwargs)
