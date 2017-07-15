efb2538c7e2e06115e746baa88c52ce3c60cbc68
Sascha Peter <sascha.o.peter@gmail.com>
2017-07-15 11:23:11 +0100

    doc: Update documentation and remove unnecessary files

--- README.md
+++ README.md
@@ -1,20 +1,25 @@
 # django-restframework-test
+
 Test project for django restframwork
 
 # Requirements: 
+
 * Python 3.4
 * Virtualenv
 
 # 3rd party requirements
+
 All python libraries are in the `requirements.txt` file.
 Install via `pip install -r requirements.txt`.
 
 # Changelog
 
 ## v0.0.1
+
 * Default setup
 * Quickstart tutorial from Django RESTframework
 
 ## v0.1.0
+
 * Snippets app
 * Serializing tutorial from Django RESTframework completed
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,4 +1,4 @@
-"""tutorial URL Configuration
+"""tutorial URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
@@ -24,7 +24,6 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    #url(r'^', include(router.urls)),
     url(r'^', include('snippets.urls')),
     url(r'^api-auth/', include('rest_framework.urls',
         namespace='rest_framework')),
